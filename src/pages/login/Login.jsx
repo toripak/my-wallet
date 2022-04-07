1a2a4f11b184bd2dc040848bf217815d282bc0ef
Victoria <vi.park6@gmail.com>
2022-04-07 17:51:17 +0200

    style login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Wrapper } from './Login.styles';
 
 export const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password)
+  }
+
   return (
-    <div>Login</div>
+    <Wrapper onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button>Login</button>
+    </Wrapper>
   )
 }
---
+++ src/pages/login/Login.styles.js
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled('form')`
+  max-width: 250px;
+  margin: 25px auto;
+  padding: 10px;
+
+  h2 {
+    font-size: 1.4rem;
+  }
+
+  label {
+    display: block;
+    margin: 15px auto;
+  }
+
+  span {
+    display: block;
+    margin: 3px;
+  }
+
+  input {
+    padding: 6px;
+    font-size: 1em;
+    color: #333;
+    width: 100%;
+    border-radius: 5px;
+  }
+
+
+`;
