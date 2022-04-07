616b0e9c382f44d73a596c927f0849c51a304f79
Victoria <vi.park6@gmail.com>
2022-04-07 18:03:32 +0200

    Style signup page

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import { Wrapper } from '../login/Login.styles';
 
 export const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName)
+  }
+
   return (
-    <div>Signup</div>
+    <Wrapper onSubmit={handleSubmit}>
+      <h2>Sign up</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Display name:</span>
+        <input
+          type="text"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+      </label>
+      <button>Sign up</button>
+    </Wrapper>
   )
 }
