c05fed2a08272d21367b9be9665d6d1f9abcf70a
Victoria <vi.park6@gmail.com>
2022-04-09 00:14:58 +0200

    add route guarding if the user is/is not authenticated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "./pages/home/Home";
 import { Login } from "./pages/login/Login";
 import { Signup } from "./pages/signup/Signup";
@@ -6,7 +6,7 @@ import { Navbar } from "./components/Navbar";
 import { useAuthContext } from "./hooks/useAuthContext";
 
 const App = () => {
-  const { authIsReady } = useAuthContext();
+  const { authIsReady, user } = useAuthContext();
 
   return (
     <>
@@ -14,9 +14,9 @@ const App = () => {
         <BrowserRouter>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="signup" element={<Signup />} />
+            <Route path="/" element={user ? <Home /> : <Navigate to="/login" replace={true} />} />
+            <Route path="login" element={!user ? <Login /> : <Navigate to="/" replace={true} />} />
+            <Route path="signup" element={!user ? <Signup /> : <Navigate to="/" replace={true} />} />
             <Route path="*" element={
               <main style={{ padding: "2rem" }}>
                 <p>Uh-oh! Page not found...</p>
