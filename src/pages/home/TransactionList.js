226b8a273cd6a9f0df01caac4ae196ca133b29e0
Victoria <vi.park6@gmail.com>
2022-04-10 21:54:50 +0200

    Add Total field

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { useFirestore } from '../../hooks/useFirestore';
 
 import { TransactionContainer } from './Home.styles';
 
-export const TransactionList = ({ transactions }) => {
+export const TransactionList = ({ transactions, total }) => {
+  const { deleteDocument } = useFirestore('transactions');
+
   const transactionLi = transactions.map(transaction => (
     <li key={transaction.id}>
       <p className='name'>{transaction.name}</p>
       <p className='amount'>CZK{transaction.amount}</p>
+      <button onClick={() => deleteDocument(transaction.id)}>X</button>
     </li>
   ))
 
   return (
     <TransactionContainer>
       {transactionLi}
+      <li style={{ marginTop: 40 }}><p className='name' style={{ textDecoration: 'underline' }}>Total:</p> <p className='amount'>CZK{total}</p></li>
     </TransactionContainer>
   )
 }
